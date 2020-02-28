a4c5eb9f7d0ae6b71d5c0f1d4684b33200d9a6f0
thomas-michels <thomasmichels.bnu@gmail.com>
2020-02-28 11:45:19 -0300

    oddOcurrences2.py 100%

--- OddOcurrencesInArray/oddOcurrences.py
+++ OddOcurrencesInArray/oddOcurrences.py
@@ -15,7 +15,7 @@ def solution(A):
             numeros[numeros.index(dic)]['ocorrencias'] += 1
 
     numero = [i for i, val in enumerate(numeros) if val['ocorrencias'] == 1]
-
+    print(numeros)
     return numeros[numero[0]]['numero']
 
 
