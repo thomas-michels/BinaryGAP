c04a72371e337afeed478cee03b12aa5848a8728
thomas-michels <thomasmichels.bnu@gmail.com>
2020-02-28 10:29:56 -0300

    oddOcurrences.py 66%

--- OddOcurrencesInArray/oddOcurrences.py
+++ OddOcurrencesInArray/oddOcurrences.py
@@ -1,21 +1,25 @@
+
 def solution(A):
-    posicoes_iguais = []
 
+    numeros = []
     for i in A:
-        pos_iguais = []
-        for j in A:
-            if i == j:
-                pos_iguais.append(j)
+        dic = {
+            'numero': i,
+            'ocorrencias': 1
+        }
 
-        if not pos_iguais in posicoes_iguais:
-            posicoes_iguais.append(pos_iguais)
+        if dic not in numeros:
+            numeros.append(dic)
 
-    nao_emparelhado = 0
-    for i in posicoes_iguais:
-        if len(i) == 1:
-            nao_emparelhado = i[0]
+        else:
+            numeros[numeros.index(dic)]['ocorrencias'] += 1
 
-    return nao_emparelhado
+    for i in numeros:
+        if i['ocorrencias'] == 1:
+            numero_so = i['numero']
+
+    return numero_so
+
 
 if __name__ == '__main__':
     print(solution([9, 3, 9, 3, 9, 7, 9]))
