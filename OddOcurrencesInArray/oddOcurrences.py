f2a4ff4492019286e08ccea35f0863462e54910c
thomas-michels <thomasmichels.bnu@gmail.com>
2020-02-28 08:56:52 -0300

    oddOcurrences.py feito

--- OddOcurrencesInArray/oddOcurrences.py
+++ OddOcurrencesInArray/oddOcurrences.py
@@ -1,38 +1,21 @@
+def solution(A):
+    posicoes_iguais = []
 
-class OddOcurrences:
+    for i in A:
+        pos_iguais = []
+        for j in A:
+            if i == j:
+                pos_iguais.append(j)
 
-    def __init__(self):
-        self.__matriz = []
+        if not pos_iguais in posicoes_iguais:
+            posicoes_iguais.append(pos_iguais)
 
-    def set_matriz(self, matriz):
-        self.__matriz = matriz
+    nao_emparelhado = 0
+    for i in posicoes_iguais:
+        if len(i) == 1:
+            nao_emparelhado = i[0]
 
-    def get_matriz(self):
-        return self.__matriz
-
-    def ver_emparelhados(self):
-        posicoes_iguais = []
-
-        for i in self.__matriz:
-            pos_iguais = []
-            for j in self.__matriz:
-                if i == j:
-                    pos_iguais.append(j)
-
-            if not pos_iguais in posicoes_iguais:
-                posicoes_iguais.append(pos_iguais)
-
-        nao_emparelhado = 0
-        for i in posicoes_iguais:
-            if len(i) == 1:
-                nao_emparelhado = i[0]
-
-        return nao_emparelhado
-
-def solucion(A):
-    odd = OddOcurrences()
-    odd.set_matriz(A)
-    return odd.ver_emparelhados()
+    return nao_emparelhado
 
 if __name__ == '__main__':
-    print(solucion([1, 2, 1, 2, 4, 1, 2]))
+    print(solution([9, 3, 9, 3, 9, 7, 9]))
