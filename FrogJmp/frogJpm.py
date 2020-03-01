0194d5e45b44624da22956e3584bf4af4fd920cc
thomas-michels <thomasmichels.bnu@gmail.com>
2020-02-29 21:53:55 -0300

    frogJpm.py 100% no codility

--- FrogJmp/frogJpm.py
+++ FrogJmp/frogJpm.py
@@ -4,12 +4,26 @@ def solution(X, Y, D):
     if X == Y:
         return 0
 
-    dist_plus = X // D
+    if Y < 100:
+        saltos = 0
+        sair = False
+        while not sair:
+            X += D
+            saltos += 1
+            if X >= Y:
+                sair = True
+                continue
 
-    saltos = (Y / D).__round__() - dist_plus
+
+        return saltos
+
+    dist_plus = X // D
+    dist_padrao = (Y / D).__round__()
+    saltos = dist_padrao - dist_plus
 
     return saltos
 
 
 if __name__ == '__main__':
     print(solution(10, 85, 30))
+    print(solution(2, 11, 3))
