c976719cf61a83196ca7c2a62ed0456654e30ee1
thomas-michels <thomasmichels.bnu@gmail.com>
2020-03-02 08:46:49 -0300

    frogJpm.py 100%

--- FrogJmp/frogJpm.py
+++ FrogJmp/frogJpm.py
@@ -4,24 +4,12 @@ def solution(X, Y, D):
     if X == Y:
         return 0
 
-    if Y < 100:
-        saltos = 0
-        sair = False
-        while not sair:
-            X += D
-            saltos += 1
-            if X >= Y:
-                sair = True
-                continue
+    pulos_real = (X - Y) // D
 
+    if pulos_real < 0:
+        pulos_real *= -1
 
-        return saltos
-
-    dist_plus = X // D
-    dist_padrao = (Y / D).__round__()
-    saltos = dist_padrao - dist_plus
-
-    return saltos
+    return pulos_real.__round__()
 
 
 if __name__ == '__main__':
